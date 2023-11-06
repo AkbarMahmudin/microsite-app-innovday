7eb33ab6ff4efeb788d19fef3819757efe325b65
AkbarMahmudin <wastanami17@gmail.com>
2023-11-06 16:10:26 +0700

    style(breadcrumbs): create breadcrumbs component

---
+++ components/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { ChevronRight } from "lucide-react";
+import React from "react";
+import { LinkItem } from ".";
+
+const Separator = ({ index }: { index: number }) => {
+  return index !== 0 ? (
+    <ChevronRight className="inline-flex w-6 h-6 text-neutral-300" />
+  ) : null;
+};
+
+type Props = {
+  links: {
+    name: string;
+    href?: string;
+  }[];
+};
+
+const Breadcrumbs = ({ links }: Props) => {
+  const linkIcons = {
+    home: "jam:home",
+  } as any;
+
+  return (
+    <div className="flex flex-row justify-start items-center md:gap-1 gap-0.5 font-medium md:text-sm text-xs">
+      {links.map((link, index) => (
+        <span
+          key={index}
+          className="flex flex-row md:gap-1 gap-0.5 justify-start items-center"
+        >
+          <Separator index={index} />
+          <LinkItem href={link.href} activeLast={index === links.length - 1}>
+            {linkIcons[link.name.toLowerCase()] ? (
+              <Icon
+                icon={linkIcons[link.name.toLowerCase()]}
+                className="h-[21px] w-[21px]"
+              />
+            ) : (
+              link.name
+            )}
+          </LinkItem>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default Breadcrumbs;
---
+++ components/breadcrumbs/link-item.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { Button, buttonVariants } from "../ui/button";
+
+type LinkItemProps = {
+  href?: string;
+  activeLast?: boolean;
+  children: React.ReactNode;
+};
+
+const LinkItem = ({ href, activeLast = false, children }: LinkItemProps) => {
+  if (!href) {
+    return <span className="text-neutral-500">{children}</span>;
+  }
+
+  if (activeLast) {
+    return (
+      <Button variant="link" size="sm" disabled>
+        {children}
+      </Button>
+    );
+  }
+
+  return (
+    <Link
+      href={href}
+      className={buttonVariants({
+        variant: "link",
+        size: "sm",
+        className: "text-neutral-500 hover:text-neutral-600",
+      })}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkItem;
