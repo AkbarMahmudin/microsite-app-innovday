c4e6317fb91d88266821298969b168f6fe42dfbe
AkbarMahmudin <wastanami17@gmail.com>
2023-11-13 11:39:11 +0700

    style(pagination): create pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24 px-24">
---
+++ components/custom/pagination/paginate.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import React from "react";
+
+import ReactPaginate from "react-paginate";
+
+type Props = {
+  pageCount: number;
+  page: number;
+  perPage: number;
+  className?: string;
+  onPageChange: (event: any) => void;
+};
+
+const Paginate = ({
+  pageCount,
+  page,
+  perPage,
+  className,
+  onPageChange,
+}: Props) => {
+  return (
+    <ReactPaginate
+      breakLabel="..."
+      nextLabel={<Icon icon="eva:chevron-right-outline" width={24} />}
+      nextClassName="py-[5px] px-[14px]"
+      onPageChange={onPageChange}
+      initialPage={page}
+      pageRangeDisplayed={perPage}
+      pageCount={pageCount}
+      previousLabel={<Icon icon="eva:chevron-left-outline" width={24} />}
+      previousClassName="py-[5px] px-[14px]"
+      renderOnZeroPageCount={null}
+      pageClassName="py-[5px] px-[14px]"
+      activeClassName="bg-primary text-white rounded-full p-0"
+      disabledLinkClassName="cursor-not-allowed opacity-50"
+      className={cn(
+        "flex justify-center items-center gap-2 text-primary font-bold md:text-base text-sm w-full flex-wrap",
+        className
+      )}
+    />
+  );
+};
+
+export default Paginate;
