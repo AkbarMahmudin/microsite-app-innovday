3d3e570bddd2f6c1383baba642a8e773f2f158d1
AkbarMahmudin <wastanami17@gmail.com>
2023-12-06 14:40:25 +0700

    config(townhall): add config cache

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     remotePatterns: [],
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
